07c5e3725e5e362ae8637ca42616eeff847aa70f
dgrotsev <dgrotsev@greet-go.com>
2017-08-23 13:22:25 +0300

    Fix test grant execute

--- db/test/init.sql
+++ db/test/init.sql
@@ -7,7 +7,7 @@ create extension if not exists "pgtap" schema board_test;
 
 alter database postgres set search_path to board_test, board;
 
+alter default privileges for role board in schema board_test grant execute on functions to public;
 grant usage on schema board_test to public;
 grant usage on schema board to public;
-grant execute on all functions in schema board_test to public;
 grant board to authenticator;
