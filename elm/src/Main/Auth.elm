63a788523d41bc0ad4a8d233dffbca2d7ed92b5a
dgrotsev <dgrotsev@greet-go.com>
2017-09-18 11:45:14 +0300

    Add Main.Auth which merges AuthTabs, Login and Register

---
+++ elm/src/Main/Auth.elm
@@ -0,0 +1,288 @@
+module Main.Auth exposing (Msg, State, init, update, view)
+
+import Bootstrap.Grid as Grid
+import Bootstrap.Tab as Tab
+import Data.Auth exposing (Auth)
+import Date exposing (Date)
+import DateTimePicker
+import Field
+import Html exposing (Html)
+import Html.Attributes as Attr
+import Postgrest
+import Rpc
+import Validate exposing (Validation)
+
+
+type alias State =
+    { login : String
+    , password : String
+    , passwordAgain : String
+    , surname : String
+    , name : String
+    , dobState : DateTimePicker.State
+    , dob : Maybe Date
+
+    --
+    , authResponse : Postgrest.Response Auth
+    , authLoading : Bool
+    , loginExistsResponse : Postgrest.Response Bool
+
+    --
+    , tabState : Tab.State
+    }
+
+
+type Msg
+    = LoginMsg String
+    | PasswordMsg String
+    | PasswordAgainMsg String
+    | SurnameMsg String
+    | NameMsg String
+    | DobMsg DateTimePicker.State (Maybe Date)
+      --
+    | LoginRequest
+    | RegisterRequest
+    | AuthResult (Postgrest.Result Auth)
+    | LoginExistsResult (Postgrest.Result Bool)
+      --
+    | TabMsg Tab.State
+
+
+init : State
+init =
+    { login = ""
+    , password = ""
+    , passwordAgain = ""
+    , surname = ""
+    , name = ""
+    , dobState = DateTimePicker.initialState
+    , dob = Just <| Date.fromTime 0 -- TODO Nothing
+
+    --
+    , authResponse = Nothing
+    , authLoading = False
+    , loginExistsResponse = Nothing
+
+    --
+    , tabState = Tab.initialState
+    }
+
+
+update : Msg -> State -> ( State, Maybe Auth, Cmd Msg )
+update msg state =
+    let
+        maybeAuth =
+            state.authResponse |> Maybe.andThen Result.toMaybe
+    in
+    case msg of
+        LoginMsg login ->
+            ( { state | login = login, loginExistsResponse = Nothing }
+            , maybeAuth
+            , Rpc.loginExists LoginExistsResult Nothing state
+            )
+
+        PasswordMsg password ->
+            ( { state | password = password }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        PasswordAgainMsg passwordAgain ->
+            ( { state | passwordAgain = passwordAgain }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        SurnameMsg surname ->
+            ( { state | surname = surname }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        NameMsg name ->
+            ( { state | name = name }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        DobMsg dobState dob ->
+            ( { state | dobState = dobState, dob = dob }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        LoginRequest ->
+            ( { state | authLoading = True }
+            , maybeAuth
+            , Rpc.login AuthResult Nothing state
+            )
+
+        RegisterRequest ->
+            ( { state | authLoading = True }
+            , maybeAuth
+            , Rpc.register AuthResult
+                Nothing
+                { state | dob = Maybe.withDefault (Date.fromTime 0) state.dob }
+            )
+
+        AuthResult result ->
+            ( { state | authLoading = False, authResponse = Just result, loginExistsResponse = Nothing }
+            , Result.toMaybe result
+            , Cmd.none
+            )
+
+        LoginExistsResult result ->
+            ( { state | loginExistsResponse = Just result }
+            , maybeAuth
+            , Cmd.none
+            )
+
+        TabMsg tabState ->
+            ( { state | tabState = tabState }, maybeAuth, Cmd.none )
+
+
+view : State -> Html Msg
+view state =
+    Grid.container [ Attr.class "mt-sm-5" ]
+        [ Grid.row []
+            [ Grid.col []
+                [ Tab.config TabMsg
+                    |> Tab.items
+                        [ Tab.item
+                            { id = "loginTab"
+                            , link = Tab.link [] [ Html.text "Вход" ]
+                            , pane = Tab.pane [] [ viewLogin state ]
+                            }
+                        , Tab.item
+                            { id = "registerTab"
+                            , link = Tab.link [] [ Html.text "Регистрация" ]
+                            , pane = Tab.pane [] [ viewRegister state ]
+                            }
+                        ]
+                    |> Tab.view state.tabState
+                ]
+            ]
+        ]
+
+
+viewLogin : State -> Html Msg
+viewLogin { login, password, passwordAgain, surname, name, dobState, dob, authResponse, authLoading, loginExistsResponse, tabState } =
+    let
+        loginExistsValidation =
+            case loginExistsResponse of
+                Just (Ok True) ->
+                    Validate.none
+
+                _ ->
+                    Validation Validate.Danger <| Just "Нет такого логина"
+
+        loginField =
+            { id = "login-login"
+            , title = "Логин"
+            , help = Just "Уникальный идентификатор пользователя"
+            , validation =
+                Validate.concat
+                    [ Validate.filled login
+                    , loginExistsValidation
+                    ]
+            , input = Field.text LoginMsg login
+            }
+
+        passwordField =
+            { id = "login-password"
+            , title = "Пароль"
+            , help = Nothing
+            , validation = Validate.secure password
+            , input = Field.password PasswordMsg password
+            }
+    in
+    Grid.container [ Attr.class "mt-sm-5" ]
+        [ Field.form authLoading authResponse LoginRequest "Войти" <|
+            [ loginField
+            , passwordField
+            ]
+        ]
+
+
+viewRegister : State -> Html Msg
+viewRegister { login, password, passwordAgain, surname, name, dobState, dob, authResponse, authLoading, loginExistsResponse, tabState } =
+    let
+        loginNotExistsValidation =
+            case loginExistsResponse of
+                Just (Ok False) ->
+                    Validate.none
+
+                _ ->
+                    Validation Validate.Danger <| Just "Логин уже существует"
+
+        loginField =
+            { id = "register-login"
+            , title = "Логин"
+            , help = Just "Уникальный идентификатор пользователя"
+            , validation =
+                Validate.concat
+                    [ Validate.filled login
+                    , loginNotExistsValidation
+                    ]
+            , input = Field.text LoginMsg login
+            }
+
+        passwordField =
+            { id = "register-password"
+            , title = "Пароль"
+            , help = Nothing
+            , validation = Validate.secure password
+            , input = Field.password PasswordMsg password
+            }
+
+        passwordAgainField =
+            let
+                validation =
+                    if password == passwordAgain then
+                        Validate.Validation Validate.Success Nothing
+                    else
+                        Validate.Validation Validate.Danger <| Just "Пароли должны совпадать"
+            in
+            { id = "passwordAgain"
+            , title = "Ещё раз пароль"
+            , help = Nothing
+            , validation = validation
+            , input = Field.password PasswordAgainMsg passwordAgain
+            }
+
+        surnameField =
+            { id = "surname"
+            , title = "Фамилия"
+            , help = Nothing
+            , validation = Validate.filled surname
+            , input = Field.text SurnameMsg surname
+            }
+
+        nameField =
+            { id = "name"
+            , title = "Имя"
+            , help = Nothing
+            , validation = Validate.none
+            , input = Field.text NameMsg name
+            }
+
+        dobField =
+            { id = "dob"
+            , title = "Дата рождения"
+            , help = Nothing
+            , validation = Validate.required dob
+
+            -- TODO use validation
+            , input = \id validation -> DateTimePicker.datePicker DobMsg [ Attr.id id ] dobState dob
+            }
+    in
+    Grid.container [ Attr.class "mt-sm-5" ]
+        [ Field.form authLoading authResponse RegisterRequest "Зарегистрироваться" <|
+            [ loginField
+            , passwordField
+            , passwordAgainField
+            , surnameField
+            , nameField
+            , dobField
+            ]
+        ]
