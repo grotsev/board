702106d24f35cf714c33dfbf2830142c1b803b60
dgrotsev <dgrotsev@greet-go.com>
2017-09-20 16:37:46 +0300

    Add doc/striaght-software-stack.md

--- elm/src/Route.elm
+++ elm/src/Route.elm
@@ -2,11 +2,93 @@ module Route exposing (Route(..), fromLocation, href)
 
 import Html exposing (Attribute)
 import Html.Attributes as Attr
+import Maybe.Extra as Maybe
 import Navigation exposing (Location)
 import UrlParser as Url exposing ((</>), s)
 import Uuid exposing (Uuid)
 
 
+--
+
+
+type R
+    = HomeR (Maybe VotingsR)
+
+
+type VotingsR
+    = VotingsR (Maybe VotingR)
+
+
+type VotingR
+    = VotingR Int (Maybe VotingEditR)
+
+
+type VotingEditR
+    = VotingEdit
+    | VotingDelete
+
+
+h : R
+h =
+    HomeR Nothing
+
+
+vs : R
+vs =
+    HomeR <//> VotingsR Nothing
+
+
+v : R
+v =
+    HomeR <//> VotingsR <//> VotingR 1 <//> VotingEdit
+
+
+infixr 7 <//>
+(<//>) : (Maybe a -> b) -> a -> b
+(<//>) x y =
+    x <| Just y
+
+
+toStr : R -> String
+toStr r =
+    let
+        cont =
+            Maybe.unwrap []
+
+        home (HomeR x) =
+            "#" :: cont votings x
+
+        votings (VotingsR x) =
+            "voting" :: cont voting x
+
+        voting (VotingR uuid x) =
+            Basics.toString uuid :: cont votingEdit x
+
+        votingEdit ve =
+            case ve of
+                VotingEdit ->
+                    [ "edit" ]
+
+                VotingDelete ->
+                    [ "delete" ]
+    in
+    String.join "/" <| home r
+
+
+
+{-
+   --
+
+
+   type RootT =
+       RootT
+
+   r: RootT
+   r =
+
+   type VotingsT =
+       VotingsT RootT
+-}
 -- CORE --
 
 
