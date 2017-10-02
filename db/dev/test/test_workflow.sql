0236a258e69e8a5bbfff24cc28d7aa5cf4eaae8d
dgrotsev <dgrotsev@greet-go.com>
2017-10-02 11:32:20 +0300

    Extend test_workflow

--- db/dev/test/test_workflow.sql
+++ db/dev/test/test_workflow.sql
@@ -8,6 +8,8 @@ declare
   voting uuid;
   option_flower uuid;
   option_cert uuid;
+  option_mars uuid;
+  option_snickers uuid;
 begin
 
   perform register('alice', 'secret', 'Лиса', 'Алиса', date '1980-01-31');
@@ -17,14 +19,19 @@ begin
   return next throws_ok($$select login('alice', 'secreZ')$$, 'A0001', 'Invalid login and password', 'Wrong pasword');
   return next is((login('alice', 'secret')).role, 'staff', 'Right pasword');
 
-  perform mock_login('alice', 'secret');
-
-  select new_voting('День рождения Веры') into voting;
-
+  -- Birthday of Alice
+  perform mock_login('bob', 'secret');
+  select new_voting('День рождения: Алиса') into voting;
   select new_option(voting, 'Цветы') into option_flower;
   select new_option(voting, 'Подарочный сертификат') into option_cert;
-
   perform new_vote(voting, option_flower);
+
+  -- Birthday of Charlie
+  perform mock_login('alice', 'secret');
+  select new_voting('День рождения: Чарли') into voting;
+  select new_option(voting, 'Марс') into option_mars;
+  select new_option(voting, 'Сникерс') into option_snickers;
+  perform new_vote(voting, option_mars);
 
 end;
 $function$;
